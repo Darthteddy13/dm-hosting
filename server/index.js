004dd6c57be77ff1c7322400c98c68fa64f014d2
Kevin Taylor <kevintay96@gmail.com>
2022-07-20 14:21:16 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require (`express`);
+const path = require (`path`);
+
+const app = express();
+
+app.get(`/`, (req, res) =>
+{
+    res.sendFile(path.join(__dirname, `../index.html`))
+})
+
+app.get(`/css`, (req, res) => res.sendFile(path.join(__dirname, `../style.css`)))
+
+app.get(`/js`, (req, res) => res.sendFile(path.join(__dirname, `../main.js`)))
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => console.log(`Server is docked on port :${port}`))
